7644a416701a1a7e786a269676af3d903c60004e
Nikitha2309 <nikithareddy2309@gmail.com>
2021-03-05 20:13:42 +0530

    form -> resume pdf

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.index,name='index'),
+    path('/home',views.index,name='home')
 ]
